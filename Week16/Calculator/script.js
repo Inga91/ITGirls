c47b50e62a6f913c5ba2965d0b0d320cd724a1d4
Inga Petrova <inga_petrova91@mail.ru>
2022-03-13 23:34:56 +0300

    added Week16

---
+++ Week16/Calculator/script.js
@@ -0,0 +1,20 @@
+add.onclick = function () {
+    let first = +prompt('First number');
+    let second = +prompt('Second number');
+    alert(first + second);
+};
+sub.onclick = function () {
+    let first = +prompt('First number');
+    let second = +prompt('Second number');
+    alert(first - second);
+};
+mul.onclick = function () {
+    let first = +prompt('First number');
+    let second = +prompt('Second number');
+    alert(first * second);
+};
+div.onclick = function () {
+    let first = +prompt('First number');
+    let second = +prompt('Second number');
+    alert(first / second);
+};
---
+++ Week16/input name/script.js
@@ -0,0 +1,4 @@
+elem.onclick = function () {
+    let name = prompt('Как Вас зовут?');
+    alert("Привет, " + name);
+};
