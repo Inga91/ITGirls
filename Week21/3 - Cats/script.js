f02c79afc6e78236b8343dc3720ac5433235510f
Inga Petrova <inga_petrova91@mail.ru>
2022-05-29 20:51:37 +0200

    added 3

---
+++ Week21/3 - Cats/script.js
@@ -0,0 +1,14 @@
+postButton.onclick = function (e) {
+   
+    e.preventDefault();
+    fetch("https://httpbin.org/post",
+    {
+        method: 'POST',
+        body: new FormData(form),
+    })
+    .then(response => response.json())
+    .then(user => {
+        console.log(user);
+    })
+    .catch(error => console.log(error));
+}
