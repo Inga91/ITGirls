259d2c0860ea5be581c759691ffaa30882f06922
Inga Petrova <inga_petrova91@mail.ru>
2022-07-04 01:50:10 +0200

    add: add week26 project studylanguages

---
+++ Week26/study-cards/src/components/Main.jsx
@@ -0,0 +1,14 @@
+
+import Language from './Language';
+import Description from './Description';
+
+function Main() {
+    return (
+        <main>
+            <Language />
+            <Description />
+        </main>
+    );
+}
+
+export default Main;
